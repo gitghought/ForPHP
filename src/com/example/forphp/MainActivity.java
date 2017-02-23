5f982c8c30ad704ad96c1d94cf18fef07a1cded2
gitghought <simpleedu.hello@gmail.com>
2017-02-23 14:56:58 +0800

    20170223：将原上传方法、下载方法放到指定的工具包中

--- src/com/example/forphp/MainActivity.java
+++ src/com/example/forphp/MainActivity.java
@@ -23,7 +23,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.util.EntityUtils;
 
-import com.example.forphp.utils.UtilDebug;
+import com.example.forphp.utils.debug.UtilDebug;
+import com.example.forphp.utils.download.UtilDownload;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -32,79 +33,6 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
-	public void sendPost(String url, String fileName) throws ClientProtocolException, IOException {
-		HttpPost objPost = new HttpPost(url);
-
-		HttpClient client = new DefaultHttpClient();
-		HttpResponse objEXE = client.execute(objPost);
-		
-		long len = objEXE.getEntity().getContentLength();
-		
-		InputStream is = objEXE.getEntity().getContent();
-		
-		File localFile = new File(fileName);
-
-		boolean ret = localFile.createNewFile();
-		if (ret == false) {
-			UtilDebug.di(
-					UtilDebug.TAG_MAINACTIVITY
-					, "-----------------create new file ----failed ==------"
-					);
-		}
-		FileOutputStream fos = new FileOutputStream(localFile);
-		byte[] buf = new byte[1024];
-		while (true) {
-			int cont = is.read(buf);
-			if (cont == -1) {
-				break;	
-			}
-			
-			UtilDebug.di(UtilDebug.TAG_MAINACTIVITY, "........");
-			fos.write(buf, 0, cont);
-		}
-		
-		fos.close();
-		is.close();
-
-		return; 
-	}
-
-	public void uploadedAPKFile(String url, String path) throws ClientProtocolException, IOException {
-		File file = new File(path);
-		if (file.exists() == false) {
-			UtilDebug.di(UtilDebug.TAG_MAINACTIVITY, "failed");
-		}
-		
-		HttpClient client = new DefaultHttpClient();
-		HttpPost postOBJ = new HttpPost(url);
-		
-		FileBody fBody = new FileBody(file);
-		MultipartEntity entity = new MultipartEntity();
-		entity.addPart("file", fBody);
-		postOBJ.setEntity(entity);
-		
-		try {
-			HttpResponse response = client.execute(postOBJ);
-			
-			int statusCode = response.getStatusLine().getStatusCode();
-			String result = EntityUtils.toString(response.getEntity(), "UTF-8");
-			
-			UtilDebug.di(UtilDebug.TAG_MAINACTIVITY, "statuscode = " + statusCode);
-			UtilDebug.di(UtilDebug.TAG_MAINACTIVITY, "result = " + result);
-			
-			 if (statusCode == 201) {
-				 UtilDebug.di(UtilDebug.TAG_MAINACTIVITY, "successed");
-			 }
-			 
-			 client.getConnectionManager().shutdown();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return ;	
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -117,7 +45,7 @@ public class MainActivity extends Activity {
 			public void run() {
 				try {
 
-					MainActivity.this.sendPost(
+					UtilDownload.sendPost(
 							"http://192.168.1.109/down.php", 
 							"/data/data/com.example.forphp/MainActivity-debug.apk"
 							);
